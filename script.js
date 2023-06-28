7e9456744f9bdc9ee5eb5e9fac7c955a64e79b09
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-06-27 23:25:33 -0500

    image change somewhat working, need to fix sizing issue

---
+++ script.js
@@ -0,0 +1,23 @@
+const imgs = document.getElementById("imgs");
+const leftBtn = document.querySelector('left');
+const rightBtn = document.querySelector('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let index = 0;
+
+let interval = setInterval(run, 2000) 
+
+function run() {
+    index++
+    changeImage()
+}
+
+function changeImage() {
+    if(index > img.length -1) {
+        index = 0
+    } else if (index < 0) {
+        index = img.length -1
+    }
+    imgs.style.transform = `translateX(${-index * 500}px)`
+}
